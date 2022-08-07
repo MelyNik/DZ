099380525ef4b25f9d827d46e660833d30a7df2b
Nikita <number_nik@mail.ru>
2022-08-07 16:37:11 +0300

    Подготовили 4 задачи

---
+++ Dz3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Пример:
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.WriteLine("Введите число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+numberA = numberA % 2;
+if ( numberA == 0 ) 
+Console.WriteLine($"Число является чётным");
+else
+{
+    Console.WriteLine($"Число не является чётным");
+}
---
+++ Dz4/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Пример:
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Выводим чётные числа:");
+for ( int i = 2; numberA > i; i += 2 )
+{
+    Console.Write($" {i} ");
+}
