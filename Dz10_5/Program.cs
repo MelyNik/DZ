2f97cea3f5fa67b6cc0f449539c8f72815a96d63
Nikita <number_nik@mail.ru>
2022-08-10 13:10:23 +0300

    Добавлены 5 и 7 задачи

---
+++ Dz10_5/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+вторую цифру этого числа.
+Пример:
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit1And2 = number / 10;
+int digit2 = digit1And2 % 10;
+
+Console.WriteLine($"{number} -> {digit2}");
+
+
+
---
+++ Dz15_7/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+Пример:
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите цифру дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>7 || number < 1)
+{
+    Console.WriteLine($"Дня недели с такой цифрой {number} не существует");
+}
+else if(number > 5 & number < 8)
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
