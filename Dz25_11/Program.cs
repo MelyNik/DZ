be689b812b9bc5b8a4b872a7c7c498ed642ed288
Nikita <number_nik@mail.ru>
2022-08-17 17:47:44 +0300

    Решение ДЗ Семинар 4

---
+++ Dz25_11/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Пример:
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Clear();
+Console.WriteLine($"Введите число А: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Введите число В: ");
+int numberB = int.Parse(Console.ReadLine());
+
+void AToTheDegreeOfB(int number1, int number2)
+{
+    int i = 0;
+    int number = number1;
+    while (i < number2 - 1)
+    {
+        number1 = number1 * number;
+        i++;
+    }
+    Console.WriteLine($"Число {number} в натуральной степени числа {number2} = {number1} ");
+}
+
+AToTheDegreeOfB(numberA, numberB);
---
+++ Dz27_12/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Пример:
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Clear();
+Console.WriteLine($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int counter1 = 0;
+int Division = 0;
+while(i<number || i>number)
+{
+    Division = number / 10;
+    counter1++;
+}
+int counter2 = 0;
+int sum = 0;
+int Remains = 0;
+while(counter2<counter1)
+{
+    Remains = number % 10;
+    number = number / 10;
+    sum = sum + Remains;
+    counter1 = counter1 - 1;
+    Console.WriteLine(sum);
+}
+Console.WriteLine(sum);
