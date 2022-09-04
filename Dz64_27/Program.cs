2d4eb002b749d7339bfa01a243f207510329173d
Nikita <number_nik@mail.ru>
2022-09-04 15:42:48 +0300

    Решение ДЗ Семинара 9

--- Dz64_27/Program.cs
+++ Dz64_27/Program.cs
@@ -5,3 +5,41 @@
 M = 1; N = 5. -> ""5, 4, 3, 2, 1""
 M = 4; N = 8. -> ""8, 7, 6, 5, 4""
 */
+
+int[] DeterminationMaximumNumber(int m, int n)
+{
+    int[] array = new int[2];
+    int max = m;
+    int min = n;
+    if (n > max)
+    {
+        max = n;
+        min = m;
+    }
+    array[0] = max;
+    array[1] = min;
+    return array; 
+}
+
+
+Random r = new Random();
+int m = r.Next(1, 9);
+Console.WriteLine($"{m} ");
+int n = r.Next(1, 9);
+Console.WriteLine($"{n} ");
+Console.WriteLine();
+
+if (m == n)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"ОШИБКА!!! Между {m} и {n} нет промежутка натуральных чисел.");
+    Console.ResetColor();
+}
+else
+{
+    int[] array = DeterminationMaximumNumber(m,n);
+    for(int i = array[0]; i > array[1]-1; i--)
+    {
+    Console.Write($"{i} \t");
+    }
+}
--- Dz66_28/Program.cs
+++ Dz66_28/Program.cs
@@ -5,3 +5,60 @@
 M = 1; N = 15 -> 56
 M = 4; N = 8. -> 18
 */
+
+int[] DeterminationMaximumNumber(int m, int n)
+{
+    int[] array = new int[2];
+    int max = m;
+    int min = n;
+    if (n > max)
+    {
+        max = n;
+        min = m;
+    }
+    array[0] = min;
+    array[1] = max;
+    return array;
+}
+
+Random r = new Random();
+int m = r.Next(1, 20);
+Console.WriteLine($"{m} ");
+int n = r.Next(1, 20);
+Console.WriteLine($"{n} ");
+Console.WriteLine();
+
+if (m == n)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"ОШИБКА!!! Между {m} и {n} нет промежутка натуральных чисел.");
+    Console.ResetColor();
+}
+else
+{
+    int[] array = DeterminationMaximumNumber(m, n);
+    if (array[0] % 2 == 0)
+    {
+        int sum = array[0];
+        int result = sum;
+        for (int i = array[0] + 2; i <= array[1]; i = i + 2)
+        {
+            sum = sum + 2;
+            result = result + sum;
+            Console.WriteLine(result);
+        }
+        Console.WriteLine($"Cумма чётных чисел натуральных элементов в промежутке от {array[0]} до {array[1]} = {result} \t");
+    }
+    else
+    {
+        int sum = array[0] + 1;
+        int result = sum;
+        for (int i = array[0] + 2; i <= array[1] - 1; i = i + 2)
+        {
+            sum = sum + 2;
+            result = result + sum;
+            Console.WriteLine(result);
+        }
+        Console.WriteLine($"Cумма чётных чисел натуральных элементов в промежутке от {array[0]} до {array[1]} = {result} \t");
+    }
+}
